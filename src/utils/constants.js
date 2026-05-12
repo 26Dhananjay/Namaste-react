c9d38fdbc83e7f3d9ae410bf2f53a0bd17c5b2e3
Dhananjay Thorat <djaythorat1991@gmail.com>
2026-05-12 12:47:44 +0530

    Made constant and mock data files separatly

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const LOGO_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const CDN_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa7zA8m3jVAlhH31dhmalKAbEAtVZeM8Qfpg&s"; 
+export const CDN_URL = "https://qrmenucreator.com/assets/logos/491hzg_1705508301.8361.png"; //"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa7zA8m3jVAlhH31dhmalKAbEAtVZeM8Qfpg&s"; 
 
 // Named export
